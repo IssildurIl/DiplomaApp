a27c39381ad04039753893e0eb0d79c91d6afea6
Ishutin Ilya Dmitrievich <rollstain@ya.ru>
2021-04-06 17:21:42 +0300

    adding photo

--- app/src/main/java/ru/sfedu/diplomaapp/backgroundActivity/CreateTask.java
+++ app/src/main/java/ru/sfedu/diplomaapp/backgroundActivity/CreateTask.java
@@ -1,17 +1,28 @@
 package ru.sfedu.diplomaapp.backgroundActivity;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.format.DateUtils;
+import android.transition.TransitionManager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 
 import java.util.Calendar;
@@ -19,9 +30,11 @@ import java.util.Calendar;
 import ru.sfedu.diplomaapp.R;
 
 public class CreateTask extends AppCompatActivity {
-
     EditText mutedAddEmployee,mutedAddProjectTo,mutedAddStatus,mutedAddTime,mutedAddPoint;
+    ImageView placeFor;
     Calendar dateAndTime= Calendar.getInstance();
+    public static final int PERMISSION_CODE = 1000;
+    public static final int IMAGE_PICK_CODE = 1001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +44,24 @@ public class CreateTask extends AppCompatActivity {
         buttons();
         setInitialDateTime();
         }
+    protected void init(){
+        mutedAddEmployee =findViewById(R.id.addEmployee);
+        mutedAddProjectTo = findViewById(R.id.addProjectTo);
+        mutedAddStatus = findViewById(R.id.addStatus);
+        mutedAddTime = findViewById(R.id.addTime);
+        mutedAddPoint = findViewById(R.id.addPoint);
+        placeFor = findViewById(R.id.placeFor);
+    }
 
     protected void colorBar() {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.silver));
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.grey));
     }
 
 
+//нажатия
     protected void buttons(){
         findViewById(R.id.returnto).setOnClickListener(v -> {
             super.onBackPressed();
@@ -50,8 +72,55 @@ public class CreateTask extends AppCompatActivity {
             setTime(v);
             setDate(v);
         });
+
+        mutedAddPoint.setOnClickListener(v -> {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    requestPermissions(permissions,PERMISSION_CODE);
+                }
+                else{
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                pickImageFromGallery();
+            }
+        });
+
+
+    }
+// галлерея
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }else{
+                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            placeFor.setVisibility(View.VISIBLE);
+            placeFor.setImageURI(data.getData());
+        }
+    }
+
+    //Дата и время
     public void setDate(View v) {
         new DatePickerDialog(CreateTask.this, d,
                 dateAndTime.get(Calendar.YEAR),
@@ -75,29 +144,19 @@ public class CreateTask extends AppCompatActivity {
     }
 
     // установка обработчика выбора времени
-    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
-            dateAndTime.set(Calendar.MINUTE, minute);
-            setInitialDateTime();
-        }
+    TimePickerDialog.OnTimeSetListener t= (view, hourOfDay, minute) -> {
+        dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        dateAndTime.set(Calendar.MINUTE, minute);
+        setInitialDateTime();
     };
 
     // установка обработчика выбора даты
-    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            dateAndTime.set(Calendar.YEAR, year);
-            dateAndTime.set(Calendar.MONTH, monthOfYear);
-            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            setInitialDateTime();
-        }
+    DatePickerDialog.OnDateSetListener d= (view, year, monthOfYear, dayOfMonth) -> {
+        dateAndTime.set(Calendar.YEAR, year);
+        dateAndTime.set(Calendar.MONTH, monthOfYear);
+        dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        setInitialDateTime();
     };
-    protected void init(){
-        mutedAddEmployee =findViewById(R.id.addEmployee);
-        mutedAddProjectTo = findViewById(R.id.addProjectTo);
-        mutedAddStatus = findViewById(R.id.addStatus);
-        mutedAddTime = findViewById(R.id.addTime);
-        mutedAddPoint = findViewById(R.id.addPoint);
-    }
+
 }
 
