60ef834bf2dcba5493a99cd2f24b122ff569ea09
Ishutin Ilya Dmitrievich <rollstain@ya.ru>
2021-04-09 14:30:46 +0300

    adding icon

--- app/src/main/java/ru/sfedu/diplomaapp/backgroundActivity/CreateTask.java
+++ app/src/main/java/ru/sfedu/diplomaapp/backgroundActivity/CreateTask.java
@@ -50,7 +50,6 @@ public class CreateTask extends AppCompatActivity {
         mutedAddStatus = findViewById(R.id.addStatus);
         mutedAddTime = findViewById(R.id.addTime);
         mutedAddPoint = findViewById(R.id.addPoint);
-        placeFor = findViewById(R.id.placeFor);
     }
 
     protected void colorBar() {
@@ -73,52 +72,8 @@ public class CreateTask extends AppCompatActivity {
             setDate(v);
         });
 
-        mutedAddPoint.setOnClickListener(v -> {
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_DENIED){
-                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                    requestPermissions(permissions,PERMISSION_CODE);
-                }
-                else{
-                    pickImageFromGallery();
-                }
-            }
-            else{
-                pickImageFromGallery();
-            }
-        });
-
-
-    }
-// галлерея
-    private void pickImageFromGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent,IMAGE_PICK_CODE);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case PERMISSION_CODE:{
-                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGallery();
-                }else{
-                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            placeFor.setVisibility(View.VISIBLE);
-            placeFor.setImageURI(data.getData());
-        }
-    }
 
     //Дата и время
     public void setDate(View v) {
